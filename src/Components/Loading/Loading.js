ba348f07c880a48c02d880b62686916a86b980db
francescoup <94630119+francescoup@users.noreply.github.com>
2022-11-30 16:38:20 +0100

    fix loading

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -5,6 +5,9 @@ const Loading = () => {
   return (
 
     <div className="loading-container">
+      
+        <h1 className='loading-title'>Covid 19 Dashboard</h1>
+      
       <div className='loading-spinner'>
         
       </div>
